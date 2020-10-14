4a4381d807a1788ca3793fe579f8dcccd240a763
LuayRahil02 <54432462+LuayRahil02@users.noreply.github.com>
2020-10-14 09:21:03 +0200

    Add files via upload

---
+++ sunny land Luay/luay sunny/Template/Base.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Base
+    {
+        protected Vector2 position;
+        Rectangle rectangle;
+
+    }
+}
---
+++ sunny land Luay/luay sunny/Template/enemy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class enemy : Base
+    {
+        ///fuck you bitch you finna get popped
+    }
+}
